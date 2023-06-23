b4175aec0f4df124061f4931be8fc0c07e8dcf80
Bruno Bachmann <bruno.bachmann@suse.com>
2023-06-23 16:53:31 -0700

    Add token crate with tests

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,17 @@
 use clap::{arg, Command};
+use log::{debug, error, info, set_max_level, Level};
 use std::fs::File;
-use std::io::{Result, BufReader};
 use std::io::BufRead;
+use std::io::{BufReader, Result};
 use std::process;
-use log::{Level, set_max_level, info, debug, error};
-use regex::Regex;
-use lazy_static::lazy_static;
+mod token;
+use token::Token;
 
 macro_rules! fatal {
     ($($arg:tt)*) => {{
         error!($($arg)*);
         process::exit(1);
     }};
-}
-
-#[derive(Debug)]
-enum Token {
-    Variable(String),
-    Int(i64),
-    Plus,
-    Minus,
-    Let,
-}
-
-impl Token {
-    fn parse_first(segment: &str) -> Option<(Token, usize)> {
-        // debug!("Checking segment {}", segment);
-        let mut result = None;
-        for (i, _) in segment.char_indices() {
-            // debug!("Checking subsegment {}", &segment[..=i]);
-            match &segment[..=i] {
-                "+" => { 
-                    return Some((Token::Plus, i));
-                },
-                "-" => { 
-                    return Some((Token::Minus, i));
-                },
-                "let" => { 
-                    return Some((Token::Let, i));
-                },
-                other => {
-                    if let Some(int_token) = Token::parse_int(&other) {
-                        // This segment is an Int. If so, we record it but continue
-                        // searching as it may contain more digits.
-                        result = Some((int_token, i));
-                    } else if let Some(var_token) = Token::parse_variable_name(&other) {
-                        // This segment is a Variable. If so, we record it but continue
-                        // searching as it may contain more characters.
-                        result = Some((var_token, i));
-                    } else if let Some(_) = result {
-                        // At this point we know that the segment ending at the
-                        // current index is invalid, so since we already have a
-                        // valid token, we should return it.
-                        return result;
-                    }
-                },
-            };
-        }
-
-        result
-    }
-
-    fn parse_int(segment: &str) -> Option<Token> {
-        match segment.parse::<i64>() {
-            Ok(i) => Some(Token::Int(i)),
-            Err(_) => None,
-        }
-    }
-
-    fn parse_variable_name(segment: &str) -> Option<Token> {
-        // Use lazy_static to avoid compiling the same regex twice
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r"[a-zA-Z_]+[a-zA-Z0-9_]*").unwrap();
-        }
-
-        match RE.is_match(segment) {
-            true => Some(Token::Variable(String::from(segment))),
-            false => None,
-        }
-    }
 }
 
 fn main() {
@@ -112,24 +45,11 @@ fn main() {
             Err(err) => fatal!("Error reading line: {}", err),
         };
 
-        let mut tokens = vec![];
-        let segments: Vec<&str> = line.split_whitespace().collect();
-        for seg in segments {
-            let mut seg_remaining = String::from(seg);
-
-            while !seg_remaining.is_empty() {
-                if let Some((token, token_len)) = Token::parse_first(&seg_remaining) {
-                    // debug!("Found token {:#?} ending on index {}", token, token_len);
-                    tokens.push(token);
-                    seg_remaining = String::from(&seg_remaining[token_len+1..]);
-                } else {
-                    fatal!("[{}] Invalid token: {}", line_num, seg);
-                }
-            }
-        }
-
+        let tokens = Token::tokenize(line.as_str());
         debug!("Tokens on line {}: {:#?}", line_num, tokens);
     }
+
+    info!("Successfully compiled \"{}\"", file_path);
 }
 
 fn open_file(file_path: &str) -> Result<BufReader<File>> {
---
+++ src/token.rs
@@ -0,0 +1,320 @@
+use lazy_static::lazy_static;
+// use log::debug;
+use regex::Regex;
+use std::result::Result;
+
+// Represents any valid token in the language.
+#[derive(Debug)]
+pub enum Token {
+    Plus,
+    Minus,
+    Equal,
+    Variable(String),
+    Int(i64),
+    Let,
+    StringLiteral(String),
+}
+
+impl PartialEq for Token {
+    fn eq(&self, other: &Self) -> bool {
+        match (&self, other) {
+            (Token::Plus, Token::Plus) => true,
+            (Token::Minus, Token::Minus) => true,
+            (Token::Let, Token::Let) => true,
+            (Token::Equal, Token::Equal) => true,
+            (Token::Variable(v1), Token::Variable(v2)) => v1 == v2,
+            (Token::StringLiteral(v1), Token::StringLiteral(v2)) => v1 == v2,
+            (Token::Int(v1), Token::Int(v2)) => v1 == v2,
+            _ => false,
+        }
+    }
+}
+
+impl Token {
+    // Breaks the given slice into a list of tokens. If the slice contains any invalid tokens,
+    // an error is returned.
+    pub fn tokenize(segment: &str) -> Result<Vec<Token>, String> {
+        let mut tokens = vec![];
+        let mut search_start: usize = 0;
+
+        while search_start < segment.len() {
+            let subseg = &segment[search_start..];
+            if let Some((token, token_end)) = Token::parse_first(subseg) {
+                // The current subsegment begins with (or is entirely) a valid token. Store it and
+                // record its end index so we can start the next search at the end of the current
+                // token.
+                tokens.push(token);
+                search_start += token_end;
+            } else {
+                // The subsegment does not begin with a valid token. This means the segment is
+                // syntactically invalid.
+                return Err(format!("Expected valid token, but got \"{}\"", subseg));
+            }
+        }
+
+        Ok(tokens)
+    }
+
+    // Finds the first valid token in the slice and the index in the slice at which the token ends.
+    // If the slice does not begin with a valid token, None will be returned.
+    pub fn parse_first(segment: &str) -> Option<(Token, usize)> {
+        let mut result = None;
+        for token_end in 1..=segment.char_indices().count() {
+            if let Some(token) = Token::parse(&segment[..token_end]) {
+                // The current subsegment is a valid token.
+                let greedy = token.is_greedy();
+                result = Some((token, token_end));
+
+                // If the current token is greedy, we should return it immediately.
+                if greedy {
+                    return result;
+                }
+            }
+        }
+
+        result
+    }
+
+    // Returns false if the token, when parsed, could possibly be a part of a larger token and true
+    // otherwise.
+    fn is_greedy(&self) -> bool {
+        match self {
+            Token::Plus | Token::Minus => true,
+            _ => false,
+        }
+    }
+
+    // Attempts to parse the given slice into a token. Returns None if the slice is not a valid
+    // token.
+    pub fn parse(segment: &str) -> Option<Token> {
+        // Check +
+        if let Some(v) = Token::parse_plus(segment) {
+            return Some(v);
+        }
+
+        // Check -
+        if let Some(v) = Token::parse_minus(segment) {
+            return Some(v);
+        }
+
+        // Check =
+        if let Some(v) = Token::parse_equal(segment) {
+            return Some(v);
+        }
+
+        // Check let
+        if let Some(v) = Token::parse_let(segment) {
+            return Some(v);
+        }
+
+        // Check variable name
+        if let Some(v) = Token::parse_variable(segment) {
+            return Some(v);
+        }
+
+        // Check int
+        if let Some(v) = Token::parse_int(segment) {
+            return Some(v);
+        }
+
+        // Check string literal
+        if let Some(v) = Token::parse_string_literal(segment) {
+            return Some(v);
+        }
+
+        None
+    }
+
+    pub fn parse_plus(segment: &str) -> Option<Token> {
+        match segment.trim() {
+            "+" => Some(Token::Plus),
+            _ => None,
+        }
+    }
+
+    pub fn parse_minus(segment: &str) -> Option<Token> {
+        match segment.trim() {
+            "-" => Some(Token::Minus),
+            _ => None,
+        }
+    }
+
+    pub fn parse_let(segment: &str) -> Option<Token> {
+        match segment.trim() {
+            "let" => Some(Token::Let),
+            _ => None,
+        }
+    }
+
+    pub fn parse_int(segment: &str) -> Option<Token> {
+        match segment.trim().parse::<i64>() {
+            Ok(i) => Some(Token::Int(i)),
+            Err(_) => None,
+        }
+    }
+
+    pub fn parse_variable(segment: &str) -> Option<Token> {
+        lazy_static! {
+            static ref VARIABLE: Regex = Regex::new(r"^[a-zA-Z_]+[a-zA-Z0-9_]*$").unwrap();
+        }
+        match VARIABLE.is_match(segment.trim()) {
+            true => Some(Token::Variable(String::from(segment.trim()))),
+            false => None,
+        }
+    }
+
+    pub fn parse_equal(segment: &str) -> Option<Token> {
+        match segment.trim() {
+            "=" => Some(Token::Equal),
+            _ => None,
+        }
+    }
+
+    pub fn parse_string_literal(segment: &str) -> Option<Token> {
+        lazy_static! {
+            static ref STRING_LITERAL: Regex = Regex::new(r#""(?:[^"\\]|\\.)*""#).unwrap();
+        }
+        match STRING_LITERAL.is_match(segment.trim()) {
+            true => Some(Token::StringLiteral(String::from(
+                segment.trim().trim_matches('"').replace("\\\"", "\""),
+            ))),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::token::Token;
+
+    #[test]
+    fn parse_let() {
+        let result = Token::parse(" let ");
+        assert_eq!(result, Some(Token::Let));
+
+        let result = Token::parse(" ??a2 ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_plus() {
+        let result = Token::parse(" + ");
+        assert_eq!(result, Some(Token::Plus));
+
+        let result = Token::parse(" aos83;2/ ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_minus() {
+        let result = Token::parse(" - ");
+        assert_eq!(result, Some(Token::Minus));
+
+        let result = Token::parse(" ao9u5424lm/ ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_equal() {
+        let result = Token::parse(" = ");
+        assert_eq!(result, Some(Token::Equal));
+
+        let result = Token::parse(" ?#li4kU#@(U* ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_int() {
+        let result = Token::parse(" 123 ");
+        assert_eq!(result, Some(Token::Int(123)));
+
+        let result = Token::parse(" 9923423 ");
+        assert_eq!(result, Some(Token::Int(9923423)));
+
+        let result = Token::parse(" ..23423;lj1 ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_variable() {
+        let result = Token::parse(" _a_2_32sdfkeFDSwre980 ");
+        assert_eq!(
+            result,
+            Some(Token::Variable(String::from("_a_2_32sdfkeFDSwre980")))
+        );
+
+        let result = Token::parse(" asr32/23 ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_string_literal() {
+        let result = Token::parse(" \"asdf\" ");
+        assert_eq!(result, Some(Token::StringLiteral(String::from("asdf"))));
+
+        let result = Token::parse(" \"say \\\"something\\\"!!\" ");
+        assert_eq!(
+            result,
+            Some(Token::StringLiteral(String::from("say \"something\"!!")))
+        );
+
+        let result = Token::parse(" \"23424?? ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn parse_first() {
+        let result = Token::parse_first(" let thing  =1  ");
+        assert_eq!(result, Some((Token::Let, 5)),);
+
+        let result = Token::parse_first("letter ");
+        assert_eq!(result, Some((Token::Variable(String::from("letter")), 7)),);
+
+        let result = Token::parse_first("let thing 234 ");
+        assert_eq!(result, Some((Token::Let, 4)));
+
+        let result = Token::parse_first("thing 234 ");
+        assert_eq!(result, Some((Token::Variable(String::from("thing")), 6)));
+
+        let result = Token::parse_first("    3784751 --");
+        assert_eq!(result, Some((Token::Int(3784751), 12)),);
+
+        let result = Token::parse_first("  =letting 435");
+        assert_eq!(result, Some((Token::Equal, 3)),);
+
+        let result = Token::parse_first("  =letting ");
+        assert_eq!(result, Some((Token::Equal, 3)),);
+
+        let result = Token::parse_first("  +++++ ");
+        assert_eq!(result, Some((Token::Plus, 3)),);
+
+        let result = Token::parse_first("  -3480 ");
+        assert_eq!(result, Some((Token::Minus, 3)),);
+
+        let result = Token::parse_first("  \"some \\\"BIG\\\" string\" ");
+        assert_eq!(
+            result,
+            Some((
+                Token::StringLiteral(String::from("some \"BIG\" string")),
+                24,
+            )),
+        );
+
+        let result = Token::parse_first(" :O#$J@#?@ ");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn tokenize() {
+        let result = Token::tokenize("let thing = 234");
+        assert_eq!(
+            result,
+            Ok(vec![
+                Token::Let,
+                Token::Variable(String::from("thing")),
+                Token::Equal,
+                Token::Int(234)
+            ]),
+        );
+    }
+}
